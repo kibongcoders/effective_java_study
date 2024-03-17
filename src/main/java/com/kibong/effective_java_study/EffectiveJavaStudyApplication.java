a8f32d74f342ff6e42862f4114bf4050c27a491e
kibongcorders <kkp02052@gmail.com>
2024-03-17 10:41:04 +0900

    study : Effective Java Init

---
+++ src/main/java/com/kibong/effective_java_study/EffectiveJavaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.kibong.effective_java_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJavaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kibong/effective_java_study/EffectiveJavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kibong.effective_java_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
