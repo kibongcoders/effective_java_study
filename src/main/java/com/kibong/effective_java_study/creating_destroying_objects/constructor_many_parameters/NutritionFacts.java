7fd342a02c231c055c0d55e2651d9230dddee360
kibongcorders <kkp02052@gmail.com>
2024-03-23 23:52:23 +0900

    study : Effective Java Constructor Many Parameters init

---
+++ src/main/java/com/kibong/effective_java_study/creating_destroying_objects/constructor_many_parameters/NutritionFacts.java
@@ -0,0 +1,36 @@
+package com.kibong.effective_java_study.creating_destroying_objects.constructor_many_parameters;
+
+// 코드 2-3 빌더 패턴 - 점층적 생성자 패턴과 자바빈즈 패턴의 장점만 취했다. (17~18쪽)
+public class NutritionFacts {
+    private int servingSize;
+    private int servings;
+    private int calories;
+    private int fat;
+    private int sodium;
+    private int carbohydrate;
+
+    public NutritionFacts(int servingSize, int servings) {
+        this(servingSize, servings, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories) {
+        this(servingSize, servings, calories, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories, int fat, int sodium) {
+        this(servingSize, servings, calories, fat, sodium, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories, int fat) {
+        this(servingSize, servings, calories, fat, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories, int fat, int sodium, int carbohydrate) {
+        this.servingSize = servingSize;
+        this.servings = servings;
+        this.calories = calories;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.carbohydrate = carbohydrate;
+    }
+}
